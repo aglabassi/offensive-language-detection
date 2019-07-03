f5ed3972f4e80fb29e51ddcd1c78b58a0cf48422
Unknown <abdel.ghani.labassi@umontreal.ca>
2019-07-03 21:41:19 +0100

    adding DL

---
+++ DL/main.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 13:25:04 2019
+
+@author: Abdel
+"""
+
+def load_src(name, fpath):
+    import os, imp
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), fpath))
+
+load_src("data_utils", "../data/utils.py" )
+
+
+import torch
+import torch.optim as optim
+from torch.nn import BCELoss
+from torch.utils.data import DataLoader
+import pandas as pd
+import gensim
+from sklearn.metrics import accuracy_score
+from data_utils import make_twitter_dataset, TwitterPreprocessorTokenizer
+from utils import train_model, narray_to_tensor_dataset, raw_texts_to_sequences
+from CNN import CNN4old
+
+#Hyperparameters
+SHUFFLE_DATASET = False
+EPOCHS = 3
+TRAIN_SIZE = 0.8
+BATCH_SIZE = 32
+NB_KRNL = 256
+KRNL_WINDOW_SIZES = [2,3]
+DROP1 = 0.3
+DROP2 = 0.3
+
+#Geting the dataset
+if SHUFFLE_DATASET:
+    make_twitter_dataset()
+    
+df = pd.read_csv( "../data/tweet_old.csv" )
+X_raw, y = df["input"], df["target"]
+
+#Getting pretrained embeddings and vocab
+model = gensim.models.KeyedVectors.load_word2vec_format('embeddings/glove.twitter.27B.100d.txt')
+vocab = model.vocab
+
+#Init tokenizer
+tokenizer = TwitterPreprocessorTokenizer(stopwords=False, stem=False)
+
+#transforming  raw texts as sequences of indexes
+X = raw_texts_to_sequences(X_raw, tokenizer=tokenizer, vocab=vocab)
+
+#Train/test split
+split_loc = int(TRAIN_SIZE * len(X))
+X_train, X_test = X[:split_loc], X[split_loc:]
+y_train, y_test = y.iloc[:split_loc], y.iloc[split_loc:]
+
+#Tensorifying
+train, test =  narray_to_tensor_dataset(X_train, X_test, y_train, y_test)
+trainloader = DataLoader(train, batch_size=BATCH_SIZE)
+
+
+#Initialising model
+net = CNN4old(len(vocab), emb_weights=torch.FloatTensor(model.vectors), nb_krnl=NB_KRNL,  window_sizes=KRNL_WINDOW_SIZES, drop1=DROP1, drop2=DROP2)
+criterion = BCELoss()
+optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+
+#Training the model
+print(net)
+for epoch in range(EPOCHS):    
+    epoch_loss, epoch_acc, epoch_loss_valid, epoch_acc_valid = train_model(net, trainloader, criterion, optimizer, accuracy_score, test)
+    print("epoch: ",epoch+1, "/", EPOCHS, "  batch_size: ", BATCH_SIZE )
+    print(f'Train_Loss: {epoch_loss:.3f} | Train_acc: {epoch_acc*100:.2f}%')
+    print(f'Valid_Loss: {epoch_loss_valid:.3f} | Valid_acc: {epoch_acc_valid*100:.2f}%')
+    print('---------------------------')
+
---
+++ DL/utils.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 24 15:17:14 2019
+
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 12:57:03 2019
+
+@author: Abdel
+"""
+
+import numpy as np
+import torch
+from torch.utils.data import TensorDataset
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+
+#Transform texts in arrays of the tokens' indexs in vocab
+def raw_texts_to_sequences(texts, tokenizer, vocab):
+        
+        tkzd_texts = [ tokenizer(text) for text in texts ]
+        
+        def get_idxs(tkzd_text):
+            sequence = []
+            for word in tkzd_text:
+                try:
+                    sequence.append(vocab[word].index)
+                except:
+                    ''                
+            return sequence
+        
+        #Getting the sequences
+        sequences = [ get_idxs(tkzd_text) for tkzd_text in tkzd_texts ]
+           
+        #Padding for getting a square matrix
+        maxlen = max([len(x) for x in sequences])
+        X = pad_sequences(sequences, padding='post', maxlen=maxlen)
+        
+        return X
+                    
+
+
+def narray_to_tensor_dataset(X_train, X_test, y_train, y_test):
+
+    inputs_train, targets_train = torch.tensor(X_train, dtype = torch.long), torch.tensor(np.array(y_train), dtype = torch.float32 ) 
+    inputs_test, targets_test = torch.tensor(X_test, dtype = torch.long), torch.tensor(np.array(y_test), dtype = torch.float32 ) 
+    
+    return TensorDataset(inputs_train, targets_train), TensorDataset(inputs_test, targets_test)
+    
+
+def train_model(net, trainloader, criterion, optimizer, metric_calculator, validation_set):
+    
+    net.train()
+    epoch_loss = 0
+    epoch_mt = 0
+    
+    for i, data in enumerate(trainloader, 0):
+        #Computing predictions
+        inputs, targets = data
+        outputs = net(inputs)
+        predictions = torch.round(outputs.detach().squeeze())
+        
+        #Updating parameters
+        optimizer.zero_grad()
+        loss = criterion(outputs.squeeze(), targets)
+        loss.backward()
+        optimizer.step()
+        
+        #metric calculation for training set
+        metric = metric_calculator(targets, predictions)
+        epoch_loss += loss.item()
+        epoch_mt += metric.item()
+        
+
+    #Normalizing metrics for training set
+    epoch_loss = epoch_loss / len(trainloader) 
+    epoch_mt = epoch_mt / len(trainloader)
+    
+    #Getting predictions for validation set
+    inputs = torch.tensor([ list(instance[0]) for instance in validation_set ])
+    targets = torch.tensor([ instance[1] for instance in validation_set ])
+    outputs = net(inputs)
+    predictions = torch.round(outputs.detach().squeeze())
+    
+    #Computing metrics for validation set
+    epoch_loss_valid = criterion(outputs.squeeze(), targets)
+    epoch_mt_valid = metric_calculator(targets,predictions)
+    
+    return epoch_loss, epoch_mt, epoch_loss_valid, epoch_mt_valid
+
+
+
+def plot_history(history):
+    
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+#    plt.subplot(1, 2, 1)
+#    plt.plot(x, acc, 'b', label='Training acc')
+#    plt.plot(x, val_acc, 'r', label='Validation acc')
+#    plt.title('Training and validation accuracy')
+#    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
